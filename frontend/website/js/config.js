6e09597ec8cde79eed9536620315fd4d30210314
Ubuntu <ubuntu@ip-172-31-22-248.ec2.internal>
2019-04-05 05:00:18 +0000

    adding site files

---
+++ frontend/website/js/config.js
@@ -0,0 +1,10 @@
+var _config = {
+cognito: {
+        userPoolId: 'us-east-1_Ezct0daMZ', // e.g. us-east-2_uXboG5pAb PLEASE noTIcE ME
+                userPoolClientId: '304g7v5pac8dmngbfinjots7bl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+                        region: 'us-east-1' // e.g. us-east-2
+         },
+api: {
+        invokeUrl: 'https://7s6fghptm4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+     }
+};
