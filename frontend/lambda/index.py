27a54257000a3e1c74ed11070b8b1cce36763c7c
darwin-s-walters <darwin.s.walters@gmail.com>
2019-04-11 07:18:42 -0400

    whoopsies

--- frontend/lambda/index.py
+++ frontend/lambda/index.py
@@ -4,6 +4,29 @@ import boto3
 import csv
 import sys, os, base64, datetime, hashlib, hmac
 
+def get_sagemaker_request(passengerGender, passengerClass):
+
+    if passengerGender == "Male":
+        genderValue = 1
+    elif passengerGender == "Female":
+        genderValue = 0
+
+    class1st = passengerClass == 1
+    class2nd = passengerClass == 2
+    class3rd = passengerClass == 3
+
+    input_csv = "6,{},1,0,0,3.625,0,0,1,0,0,{},{},{},0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0".format(genderValue,class1st,class2nd,class3rd )
+    print(input_csv)
+    res = sagemaker.invoke_endpoint(
+                    EndpointName='dev-sm-endpoint',
+                    Body=input_csv,
+                    ContentType='text/csv',
+                    Accept='Accept'
+                )
+    return res['Body'].read()
+
+
+
 def lambda_handler(event, context):
     # TODO implement
     eventBody = json.loads(event['body'])
@@ -25,25 +48,3 @@ def lambda_handler(event, context):
         }
         
     }
-
-def get_sagemaker_response(passengerGender, passengerClass):
-
-    if passengerGender == "Male":
-        genderValue = 1
-    elif passengerGender == "Female":
-        genderValue = 0
-
-    class1st = passengerClass == 1
-    class2nd = passengerClass == 2
-    class3rd = passengerClass == 3
-
-    input_csv = "6,{},1,0,0,3.625,0,0,1,0,0,{},{},{},0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0".format(genderValue,class1st,class2nd,class3rd )
-    print(input_csv)
-    res = sagemaker.invoke_endpoint(
-                    EndpointName='dev-sm-endpoint',
-                    Body=input_csv,
-                    ContentType='text/csv',
-                    Accept='Accept'
-                )
-    return res['Body'].read()
-
