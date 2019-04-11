9f78ccdc63135b6252aae772f42ae431b5947c70
Sean Joo <sean.joo@gmail.com>
2019-04-11 15:19:04 -0400

    update model name

--- jenkins/jobs/config/seed-repo-config.groovy
+++ jenkins/jobs/config/seed-repo-config.groovy
@@ -11,7 +11,6 @@ repos {
         all {
             reposToAutomate = [
                [projectName: "frontend", viewName: "Web App", url: "https://github.com/ICFI/dragon.git", credentials: "", seed_job: "frontend/jobs/seed_jobs.groovy"],
-               [projectName: "web-application", viewName: "Web App", url: "https://github.com/ICFI/dragon.git", credentials: "", seed_job: "frontend/jobs/seed_jobs.groovy"],
                [projectName: "sagemaker", viewName: "SageMaker Model", url: "https://github.com/ICFI/dragon.git", credentials: "", seed_job: "sagemaker/jobs/seed_jobs.groovy"]
             ]
         }
