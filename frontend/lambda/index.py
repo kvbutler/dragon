f2a4cb7b028b29c7a5480d7309865bb17527a6cb
darwin-s-walters <darwin.s.walters@gmail.com>
2019-04-11 07:36:55 -0400

    using double quotes and consolidating lambda

--- frontend/lambda/index.py
+++ frontend/lambda/index.py
@@ -4,7 +4,11 @@ import boto3
 import csv
 import sys, os, base64, datetime, hashlib, hmac
 
-def get_sagemaker_request(passengerGender, passengerClass):
+def lambda_handler(event, context):
+    # TODO implement
+    eventBody = json.loads(event['body'])
+    print(eventBody)
+
 
     if passengerGender == "Male":
         genderValue = 1
@@ -12,39 +16,29 @@ def get_sagemaker_request(passengerGender, passengerClass):
         genderValue = 0
 
     class1st = passengerClass == 1
+    print(class1st)
     class2nd = passengerClass == 2
+    print(class2nd)
     class3rd = passengerClass == 3
-
+    print(classrd)
+    
     input_csv = "6,{},1,0,0,3.625,0,0,1,0,0,{},{},{},0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0".format(genderValue,class1st,class2nd,class3rd )
     print(input_csv)
+    sagemaker = boto3.client('sagemaker-runtime')
     res = sagemaker.invoke_endpoint(
                     EndpointName='dev-sm-endpoint',
                     Body=input_csv,
                     ContentType='text/csv',
                     Accept='Accept'
                 )
-    return res['Body'].read()
-
-
-
-def lambda_handler(event, context):
-    # TODO implement
-    eventBody = json.loads(event['body'])
-    print(eventBody)
-
-    didPassengerSurvive = get_sagemaker_request(eventBody['gender'], eventBody['passengerClass'])
-
-    if didPassengerSurvive == 1:
-        passenger = "Alive"
-    elif didPassengerSurvive == 0:
-        passenger = "Dead"
+    # return res['Body'].read()
 
     return {
         'statusCode': 201,
-        'body' : json.dumps({"passengerStatus": passenger}),
+        'body' : json.dumps({"passengerAlive": res['Body'].read()}),
         'headers' : {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         }
         
-    }
+   }
--- frontend/website/js/titanic.js
+++ frontend/website/js/titanic.js
@@ -20,7 +20,7 @@ WildRydes.map = WildRydes.map || {};
             method: 'POST',
             url: _config.api.invokeUrl + '/get_results',
             crossDomain: true,
-            headers: { 'Authorization': 'authToken', 'Access-Control-Allow-Origin': '*' },
+            headers: { "Authorization": "authToken", "Access-Control-Allow-Origin": "*" },
             data: JSON.stringify({
                 "gender": passengerGender,
                 "passengerClass": passengerClass}
