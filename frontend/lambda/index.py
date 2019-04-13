21588d35a6c5e9373f2bff9b4807d1bbe310b299
Sean Joo <sean.joo@gmail.com>
2019-04-12 23:06:24 -0400

    test

--- frontend/lambda/index.py
+++ frontend/lambda/index.py
@@ -10,35 +10,45 @@ def lambda_handler(event, context):
     print(eventBody)
 
 
-    if passengerGender == "Male":
-        genderValue = 1
-    elif passengerGender == "Female":
-        genderValue = 0
-
-    class1st = passengerClass == 1
-    print(class1st)
-    class2nd = passengerClass == 2
-    print(class2nd)
-    class3rd = passengerClass == 3
-    print(classrd)
-    
-    input_csv = "6,{},1,0,0,3.625,0,0,1,0,0,{},{},{},0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0".format(genderValue,class1st,class2nd,class3rd )
-    print(input_csv)
-    sagemaker = boto3.client('sagemaker-runtime')
-    res = sagemaker.invoke_endpoint(
-                    EndpointName='dev-sm-endpoint',
-                    Body=input_csv,
-                    ContentType='text/csv',
-                    Accept='Accept'
+    # if passengerGender == "Male":
+    #     genderValue = 1
+    # elif passengerGender == "Female":
+    #     genderValue = 0
+    #
+    # class1st = passengerClass == 1
+    # print(class1st)
+    # class2nd = passengerClass == 2
+    # print(class2nd)
+    # class3rd = passengerClass == 3
+    # print(classrd)
+    #
+    # input_csv = "6,{},1,0,0,3.625,0,0,1,0,0,{},{},{},0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0".format(genderValue,class1st,class2nd,class3rd )
+    # print(input_csv)
+    # sagemaker = boto3.client('sagemaker-runtime')
+    # res = sagemaker.invoke_endpoint(
+    #                 EndpointName='dev-sm-endpoint',
+    #                 Body=input_csv,
+    #                 ContentType='text/csv',
+    #                 Accept='Accept'
                 )
     # return res['Body'].read()
+   #
+   #  return {
+   #      'statusCode': 201,
+   #      'body' : json.dumps({"passengerAlive": res['Body'].read()}),
+   #      'headers' : {
+   #          'Content-Type': 'application/json',
+   #          'Access-Control-Allow-Origin': '*'
+   #      }
+   #
+   # }
 
     return {
         'statusCode': 201,
-        'body' : json.dumps({"passengerAlive": res['Body'].read()}),
+        'body' : json.dumps({"passengerAlive": "0"}),
         'headers' : {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         }
-        
+
    }
